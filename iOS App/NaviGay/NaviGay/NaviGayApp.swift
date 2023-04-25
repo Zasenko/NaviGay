f0ec54cf8f06513fb87fcd96f21f0078fe504b02
Zasenko <dmitry.zasenko@gmail.com>
2023-04-25 18:14:04 +0200

    add empty project

---
+++ iOS App/NaviGay/NaviGay/NaviGayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviGayApp.swift
+//  NaviGay
+//
+//  Created by Dmitry Zasenko on 25.04.23.
+//
+
+import SwiftUI
+
+@main
+struct NaviGayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
